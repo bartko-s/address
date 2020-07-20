d985388fc63c35fe9affad0e3f711f0189435a77
Bartko Stefan <bartko.stefan@gmail.com>
2020-07-20 21:17:30 +0200

    UPDATE - dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -1,10 +1,10 @@
 <?php
 
-return array(
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => array('all' => true),
-    Symfony\Bundle\TwigBundle\TwigBundle::class => array('all' => true),
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => array('dev' => true, 'test' => true),
-    Symfony\Bundle\MonologBundle\MonologBundle::class => array('all' => true),
-    Symfony\Bundle\DebugBundle\DebugBundle::class => array('dev' => true, 'test' => true),
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => array('all' => true),
-);
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+];
